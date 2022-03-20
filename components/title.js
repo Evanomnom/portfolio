da66236635fcdf98e2ceace8c9fcbe33764f9875
Evanomnom <ehemming99@gmail.com>
2022-03-20 02:15:45 -0400

    first real commit
    + added title and about me page

---
+++ components/title.js
@@ -0,0 +1,5 @@
+export default function Title() {
+    return (
+        <span className="text-3xl font-bold"></span>
+    )
+}
---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Montserrat"></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
